16a187d8c4b00ec44cca3689e1227baf29272e6e
bhoiganesh218-blip <bhoiganesh218@gmail.com>
2026-04-07 16:10:37 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
-self.addEventListener("install", () => {
-  console.log("SW Installed");
+self.addEventListener("install", (event) => {
+  self.skipWaiting(); // force activate
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim()); // control pages
 });
 
 self.addEventListener("fetch", () => {});
