d0dbd6653b2c36f3e5aee6986a79c5868ee16f2c
bhoiganesh218-blip <bhoiganesh218@gmail.com>
2026-04-07 17:19:29 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,33 @@
+const CACHE_NAME = "narrato-cache-v1";
+const urlsToCache = [
+  "/NARRATO/",
+  "/NARRATO/index.html",
+  "/NARRATO/style.css",
+  "/NARRATO/Script/app.js"
+];
+
+// INSTALL
 self.addEventListener("install", (event) => {
-  self.skipWaiting(); // force activate
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim()); // control pages
+  event.waitUntil(
+    clients.claim()
+  );
 });
 
-self.addEventListener("fetch", () => {});
+// FETCH (IMPORTANT)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
