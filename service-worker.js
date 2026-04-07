26e16c7e4e82b6cad663336c20ef7492f5b6344c
bhoiganesh218-blip <bhoiganesh218@gmail.com>
2026-04-07 17:32:34 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,24 @@
+const CACHE_NAME = "narrato-v1";
+
 self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "/NARRATO/",
+        "/NARRATO/index.html",
+        "/NARRATO/style.css"
+      ]);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
