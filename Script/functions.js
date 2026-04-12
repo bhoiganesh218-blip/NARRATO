390ff0085a12b0aa69c4998359fa87681e837de8
bhoiganesh218-blip <bhoiganesh218@gmail.com>
2026-04-13 00:01:00 +0530

    Update functions.js

--- Script/functions.js
+++ Script/functions.js
@@ -49,7 +49,6 @@ export async function init() {
   }
   // ===  WHOLE UI RRNDER === //
   
-  document.getElementById("loader").style.display = "none"; // REMOVE LODING
   
  // window.render("home")//RENDER HOME
   
